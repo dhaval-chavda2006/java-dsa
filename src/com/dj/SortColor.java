263c400754dcadc55477107cb6f22482ce0f3ccc
dhaval-chavda2006 <dhavalchavda866@gmail.com>
2026-08-27 19:58:41 +0530

    three-pointer

---
+++ src/com/dj/SortColor.java
@@ -0,0 +1,29 @@
+package com.dj;
+import java.util.*;
+
+public class SortColor {
+    //    nums = [2,0,2,1,1,0]
+    //    Output: [0,0,1,1,2,2]
+
+    public static void main(String[] Args)
+    {
+        int[] arr = {2,0,1};
+        ColorSort(arr);
+        System.out.println(Arrays.toString((arr)));
+    }
+    static void ColorSort(int[] arr)
+    {
+        for(int i =0; i< arr.length; i++)
+        {
+            for(int j = i+1; j< arr.length; j++)
+            {
+                if(arr[j]<arr[i])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
